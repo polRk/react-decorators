2dd8769a398bbd35f8d0abba967f040cb1a9f042
Vladislav Polyakov <polrkov.tm@gmail.com>
2019-04-20 20:08:17 +0300

    fix: поправил баг с запуском тестов
    Повысил права на файл cli и сделал его выполняемым прямо в файле теста

--- src/__tests__/cli.test.ts
+++ src/__tests__/cli.test.ts
@@ -1,22 +1,16 @@
 import * as execa from 'execa'
-import * as path from 'path'
-
-const oldCwd = process.cwd
 
 describe('Check file generation', () => {
   beforeEach(async () => {
     await execa('mkdir', ['test'])
-    await execa('npm', ['link'])
-    process.cwd = () => path.join(oldCwd(), 'test')
   })
 
   afterAll(async () => {
     await execa('rm', ['-rf', 'test'])
-    await execa('npm', ['unlink'])
-    process.cwd = () => oldCwd()
   })
 
-  it('should has all files', async () => {
+  it('should started', async () => {
+    await execa('chmod', ['a+x', 'lib/cli.js'])
     const { code } = await execa('wizard')
     expect(code).toBe(0)
   })
