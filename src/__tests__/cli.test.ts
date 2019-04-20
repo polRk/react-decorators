8f46b57ba4b4226398057aaa93b40c6708b6e879
Vladislav Polyakov <polrkov.tm@gmail.com>
2019-04-20 19:12:52 +0300

    feat: добавил тесты

---
+++ src/__tests__/cli.test.ts
@@ -0,0 +1,23 @@
+import * as execa from 'execa'
+import * as path from 'path'
+
+console.log(process.cwd())
+
+const oldCwd = process.cwd
+
+describe('Check file generation', () => {
+  beforeEach(async () => {
+    await execa('mkdir', ['test'])
+    process.cwd = () => path.join(oldCwd(), 'test')
+  })
+
+  afterAll(async () => {
+    await execa('rm', ['-rf', 'test'])
+    process.cwd = () => oldCwd()
+  })
+
+  it('should has all files', async () => {
+    const { code } = await execa('wizard')
+    expect(code).toBe(0)
+  })
+})
--- src/cli.ts
+++ src/cli.ts
@@ -82,6 +82,22 @@ const tasks = new Listr([
           err => err && observer.error(err)
         )
 
+        observer.next('jest.config.js')
+        fs.writeFile(
+          path.join(process.cwd(), 'jest.config.js'),
+          jsonSave(
+            (module.exports = {
+              roots: ['<rootDir>/src'],
+              transform: {
+                '^.+\\.tsx?$': 'ts-jest',
+              },
+              testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+              moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+            })
+          ),
+          err => err && observer.error(err)
+        )
+
         observer.next('.gitignore')
         fs.writeFile(
           path.join(process.cwd(), '.gitignore'),
